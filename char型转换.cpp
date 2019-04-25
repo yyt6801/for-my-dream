5d31129738c2d481b6b6714b3a4dc3954bad85fd
yyt6801 <yyt6801@gmail.com>
2019-04-25 13:16:28 +0800

    add CharChange

---
+++ char型转换.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include "string.h"
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char aa[]="HelloWorld!";					//char[]
+	printf("%s\n",aa);
+	printf("%c+%c\n",aa[4],aa[5]);
+
+	char *b=aa;							//char[] -> char*
+	printf("%s\n",b);
+
+	const char *c=b;					//char* -> const char*
+	printf("%s\n",c);
+
+	char *d;
+	d=const_cast<char*>(c);				//const char* -> char*
+	printf("%s\n",d);
+	strcpy(d,c);						//const char* -> char*
+	printf("%s\n",d);
+
+	char *m;
+	m="2019-04-18 13:27:08";
+	printf("%s\n",m);
+
+	system("pause");
+
+	return 0;
+
+}
