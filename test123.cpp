f3bb6b1aa495a8471650cea11fe7279802ccdd8e
yyt6801 <yyt6801@gmail.com>
2019-04-28 18:55:43 +0800

    test123.cpp add strncpy

--- test123.cpp
+++ test123.cpp
@@ -51,54 +51,59 @@
 //}
 
 
-//#include <iostream>
-//using namespace std;
-//int main()
-//{
-//	#define size 15//
-//	char a[size]= "hello_world";
-//	cout<<a << endl;
-//	return 0;
-//	cin.get();
-//
-//}
-
-
-//通过互斥对象句柄检测程序是否运行
-#include <stdio.h>
-#include <Windows.h>
+//使用strncpy(sub,string+12,13)/*将string中从string[12]开始的13个数字复制到sub中*/
 #include <iostream>
+#include "string.h"
 using namespace std;
+int main()
+{
+	#define size 200//
+	char a[size];
+	memset(a,0x00,sizeof(a));
+	char string[25]= "2007-12-20 18:31:34:123";
+	strncpy(a,string,19);
+	cout<<a<< endl;
+	system ("pause");
+	return 0;  
 
-char IPname[20];
-	BOOL IsAlreadyRun()
-	{
-		HANDLE hMutex = NULL;
-		hMutex = ::CreateMutex(NULL, FALSE, IPname);
-		if (hMutex)
-		{
-			if(ERROR_ALREADY_EXISTS == ::GetLastError())
-			{
-				return TRUE;
-			}
-		}
-		return FALSE;
+}
 
 
-	}
-	int	main()
-	{
-		while(1)
-		{
-			cout << "输入互斥对象的名称:" << endl;
-			cin >> IPname;
-			if(IsAlreadyRun())
-			{
-				printf("alreadyRun!!!");
-			}
-			else 
-			{
-				printf("NOTALREADYRUN!!!!");
-			}
-		}
-	}
+// //通过互斥对象句柄检测程序是否运行
+// #include <stdio.h>
+// #include <Windows.h>
+// #include <iostream>
+// using namespace std;
+
+// char IPname[20];
+// 	BOOL IsAlreadyRun()
+// 	{
+// 		HANDLE hMutex = NULL;
+// 		hMutex = ::CreateMutex(NULL, FALSE, IPname);
+// 		if (hMutex)
+// 		{
+// 			if(ERROR_ALREADY_EXISTS == ::GetLastError())
+// 			{
+// 				return TRUE;
+// 			}
+// 		}
+// 		return FALSE;
+
+
+// 	}
+// 	int	main()
+// 	{
+// 		while(1)
+// 		{
+// 			cout << "输入互斥对象的名称:" << endl;
+// 			cin >> IPname;
+// 			if(IsAlreadyRun())
+// 			{
+// 				printf("alreadyRun!!!");
+// 			}
+// 			else 
+// 			{
+// 				printf("NOTALREADYRUN!!!!");
+// 			}
+// 		}
+// 	}
