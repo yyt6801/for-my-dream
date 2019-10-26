785907d623a8a36b0cca852a60334b61eaaa3da5
yyt6801 <yyt6801@gmail.com>
2019-10-26 15:59:20 +0800

    add assembly

---
+++ Assembly/test.s
@@ -0,0 +1,45 @@
+	.file	"test.c"
+	.text
+	.globl	_add_a_and_b
+	.def	_add_a_and_b;	.scl	2;	.type	32;	.endef
+_add_a_and_b:
+LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	8(%ebp), %edx
+	movl	12(%ebp), %eax
+	addl	%edx, %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE0:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	call	___main
+	movl	$3, 4(%esp)
+	movl	$2, (%esp)
+	call	_add_a_and_b
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE1:
+	.ident	"GCC: (MinGW.org GCC-8.2.0-3) 8.2.0"
