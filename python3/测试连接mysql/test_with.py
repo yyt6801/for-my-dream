642a575a13bd70f095dfa21fa8ed9550effbf7fc
yyt6801 <yyt6801@gmail.com>
2019-12-13 20:51:45 +0800

    add python connect mysql

---
+++ python3/测试连接mysql/test_with.py
@@ -0,0 +1,56 @@
+# 操作数据库与操作文件类似，在读取修改开始和结束时都需要进行连接（打开），断开（关闭）等固定操作，文件读写时可以使用 with （上下文管理器）来简化操作，数据库当然也是可以的：
+
+# #!/usr/bin/env python
+# # -*- coding:utf-8 -*-
+
+# #  以 pymysql 为例，实现通过 with 简化数据库操作
+
+import pymysql
+
+
+class DB():
+    def __init__(self,
+                 host='192.168.142.136',
+                 port=3306,
+                 db='test001',
+                 user='root',
+                 passwd='123456',
+                 charset='utf8'):
+        # 建立连接
+        self.conn = pymysql.connect(host=host,
+                                    port=port,
+                                    db=db,
+                                    user=user,
+                                    passwd=passwd,
+                                    charset=charset)
+        # 创建游标，操作设置为字典类型
+        self.cur = self.conn.cursor(cursor=pymysql.cursors.DictCursor)
+
+    def __enter__(self):
+        # 返回游标
+        return self.cur
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # 提交数据库并执行
+        self.conn.commit()
+        # 关闭游标
+        self.cur.close()
+        # 关闭数据库连接
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    # db = pymysql.connect("qdm712457164.my3w.com", "qdm712457164", "Yuan521175", "qdm712457164_db")
+
+    # with DB() as db:
+    with DB(host='qdm712457164.my3w.com',
+            user='qdm712457164',
+            passwd='Yuan521175',
+            db='qdm712457164_db') as db:
+
+        db.execute(
+            'select table_name from information_schema.`TABLES` where table_schema= \'qdm712457164_db\''
+        )  # 表名区分大小写
+        print(db)
+        for i in db:
+            print(i)
