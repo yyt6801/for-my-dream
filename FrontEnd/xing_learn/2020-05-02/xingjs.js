a83e4916b07e9572baa66a561db60783dacbde94
yyt6801 <yyt6801@gmail.com>
2020-05-03 16:57:43 +0800

    add xing_learn

---
+++ FrontEnd/xing_learn/2020-05-02/xingjs.js
@@ -0,0 +1,22 @@
+function xing(){
+    alert("宝宝点击了按钮")
+}
+
+function xing123(){
+    alert("宝宝点击了按钮123")
+}
+
+function xing444(){
+    document.getElementById("xing").innerHTML = 9999e9;
+    alert(document.getElementById("xing").innerHTML);
+    
+       
+} 
+
+
+// a={
+//     firstName:"John",      json  键值对   key: value      a.age=50
+//     lastName:"Doe", 
+//     age:50, 
+//     eyeColor:"blue"
+// }
