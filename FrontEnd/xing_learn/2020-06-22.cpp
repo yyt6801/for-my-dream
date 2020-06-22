79df13fcbcb41dc4eea05a2104b2f595a9094f4d
yyt6801 <yyt6801@gmail.com>
2020-06-23 00:01:11 +0800

    add learn xing

--- FrontEnd/xing_learn/2020-06-22.cpp
+++ FrontEnd/xing_learn/2020-06-22.cpp
@@ -2,10 +2,28 @@
 
 int main()
 {
-    int a = 123;
-    int b =3;
-    int* p = &a;
-    int *m = &b;
-    printf("%d",&m);
+    char a = 'k';
+    char zhaomingxing = 'b'; //char 类型    字符型  只能存放一位字符
+    char *c = "zhaomingxingaibaba"; //char* 字符串类型，
+    int b = 1234; //整形  
+    printf("%c \n",a); //char 字符型 用%c输出/表示
+    printf("%d \n",b); //int 整形  用%d输出/表示   %o 八    %x十六进制
+    printf("%s \n",c);//字符串类型，用%s输出
+
+    char d[6] ="ax5hy"; //字符串的正常表示形式，字符串d包含20个字符
+    // d[0] = 'a';
+    // d[1] = 'x';
+    // d[2] = '5';
+    // d[3] = 'h';
+    // d[4] = 'y';
+    // d[5] ='\n';
+    printf("%s\n",d);
+
+
+    // //数组，是好多个元素的集合
+    // int xing[100];
+    // xing[0] = 34566543;
+    // xing[1] = 23456;
+
 
 }
