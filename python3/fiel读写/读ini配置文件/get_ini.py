d972c95557d9c76aa745d493af2643361c5e66b8
yyt6801 <1725056801@qq.com>
2021-09-23 13:42:18 +0800

    添加python读取ini配置文件

---
+++ python3/fiel读写/读ini配置文件/get_ini.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import configparser
+import os
+curpath = os.path.dirname(os.path.realpath(__file__))
+cfgpath = os.path.join(curpath, "config.ini")
+print(cfgpath)  # cfg.ini的路径
+# 创建管理对象
+conf = configparser.ConfigParser()
+ 
+# 读ini文件
+conf.read(cfgpath, encoding="utf-8")  # python3
+ 
+# 获取所有的section
+sections = conf.sections()
+print(sections)  # 返回list
+ 
+items = conf.items('section0')
+print(items)  # list里面对象是元祖
