59664f9682f7d961a8bcf8673e648703b4f1643c
yyt6801 <1725056801@qq.com>
2022-01-14 09:12:45 +0800

    add connect sqlserver in python

---
+++ ProgrammingLanguage/python3/测试连接SQLServer/test_sqlserver.py
@@ -0,0 +1,18 @@
+﻿# _*_coding:utf-8_*_
+import pymssql
+
+conn = pymssql.connect(host='10.16.16.17',database='master',user='iqdsdb2',password='Iqds_bkd')
+cur = conn.cursor()
+if not cur:
+    raise(NameError,"connect database fails") 
+else:
+    cur.execute("select getdate()")
+    data = cur.fetchall()
+
+    if data :
+        print("ok")
+        for i in data:
+            print(i)
+    else:
+        print("fail")
+    conn.close()
