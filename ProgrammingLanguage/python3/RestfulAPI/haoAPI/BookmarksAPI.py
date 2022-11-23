97d108a636bbce69850799bd7bd2094ebe8cbbc8
Kevin Yuan <1725056801@qq.com>
2022-11-24 01:20:14 +0800

    update BookmarksAPI.py

--- ProgrammingLanguage/python3/RestfulAPI/haoAPI/BookmarksAPI.py
+++ ProgrammingLanguage/python3/RestfulAPI/haoAPI/BookmarksAPI.py
@@ -1,19 +1,27 @@
+# # -*- coding:utf-8 -*-
 #!flask/bin/python
 from flask import Flask, jsonify
 from flask import abort
 from flask import make_response
 from flask import request
 from USE_DB import DB  # 调用另一个python中的类和函数， from a import b    a.py不需要加.py
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app, resources=r'/*')	# 注册CORS, "/*" 允许访问所有api
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "hello world!"
 
 @app.route('/test', methods=['GET'])
 def get_test():
     return "hello world!"
 
 #获取数据库查询结果并返回
-@app.route('/get_json', methods=['GET'])
+@app.route('/get_json', methods=['POST'])
 def get_tasks():
     # with DB() as db:   通过调用DB()函数，把返回的数据给as后面的db
     with DB() as db:
@@ -45,4 +53,5 @@ def create_task():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0',port=81)
